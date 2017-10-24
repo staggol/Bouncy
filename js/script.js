37fe7acaa28f3689e892afa8d4ba8540b166eabe
Staggol <gameslegendblood@gmail.com>
2017-10-24 22:38:55 +0300

    Few blocks

--- js/script.js
+++ js/script.js
@@ -25,3 +25,18 @@ document.addEventListener('DOMContentLoaded', function () {
   }
 
 });
+
+// Якорные ссылки
+
+$(document).ready(function() {
+    $('a[href^="#"]').click(function () { 
+    elementClick = $(this).attr("href");
+        destination = $(elementClick).offset().top;
+        if($.browser.safari){
+        $('body').animate( { scrollTop: destination }, 1100 );
+        }else{
+        $('html').animate( { scrollTop: destination }, 1100 );
+        }
+        return false;
+    });
+ });

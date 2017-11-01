a7e304b4917a85c286359f02b63156aab18de11f
Staggol <gameslegendblood@gmail.com>
2017-11-01 19:38:49 +0300

    slowScroll

--- js/script.js
+++ js/script.js
@@ -28,15 +28,10 @@ document.addEventListener('DOMContentLoaded', function () {
 
 // Якорные ссылки
 
-$(document).ready(function() {
-    $('a[href^="#"]').click(function () { 
-    elementClick = $(this).attr("href");
-        destination = $(elementClick).offset().top;
-        if($.browser.safari){
-        $('body').animate( { scrollTop: destination }, 1100 );
-        }else{
-        $('html').animate( { scrollTop: destination }, 1100 );
-        }
-        return false;
-    });
- });
+function slowScroll(id) {
+	var offset = 0;
+	$('html, body').animate ({
+		scrollTop: $(id).offset ().top - offset
+	}, 500);
+	return false;
+}
